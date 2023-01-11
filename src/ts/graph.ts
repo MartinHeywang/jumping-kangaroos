a49524be339373d0928f2c58d8877efd4701e6e1
Martin Heywang <martin.heywang1@gmail.com>
2023-01-11 17:08:31 +0100

    Drawn the kangaroos position

--- src/ts/graph.ts
+++ src/ts/graph.ts
@@ -1,17 +1,41 @@
 const canvas = document.querySelector<HTMLCanvasElement>(".simulation__graphic")!;
 const ctx = canvas.getContext("2d")!;
 
-const minX = -1;
-const maxX = 15;
+let minX = -1;
+let maxX = 15;
+let minY = -1;
+let maxY = 15;
 
-const minY = -1;
-const maxY = 15;
+// quotient of d0 / d1
+const s = 56/23;
+
+let kangarooPositions: {
+    C0: { x: number }[];
+    C1: { x: number }[];
+};
+
+// the chased kangaroo's movement
+const C1 = (t: number) => 1 + t;
 
 export function initGraphSimulation() {
     canvas.width = canvas.parentElement!.clientWidth;
     canvas.height = canvas.parentElement!.clientHeight;
 
+    computeKangarooPositions();
+
+    const allPositions = kangarooPositions.C0.concat(kangarooPositions.C1);
+    minX = allPositions.reduce((previous, pos) => {
+        return previous.x < pos.x ? previous : pos;
+    }).x - 1;
+    maxX = allPositions.reduce((previous, pos) => {
+        return previous.x > pos.x ? previous : pos;
+    }).x + 1;
+    
+    minY = -1;
+    maxY = kangarooPositions.C0.length; // both C0 and C1 have the same number of positions
+
     drawAxis();
+    drawKangaroos();
 }
 
 function drawAxis() {
@@ -35,11 +59,7 @@ function drawAxis() {
 
     const textOffset = 6;
 
-    ctx.fillText(
-        "0",
-        canvasOrigin.x - textOffset,
-        canvasOrigin.y + textOffset
-    );
+    ctx.fillText("0", canvasOrigin.x - textOffset, canvasOrigin.y + textOffset);
 
     // draw the grads
     const gradLength = 14;
@@ -68,6 +88,72 @@ function drawAxis() {
 
         ctx.fillText(y.toString(), canvasOrigin.x - gradLength / 2 - textOffset, canvasY);
     }
+}
+
+function computeKangarooPositions() {
+    // initial positions given with the problem
+    kangarooPositions = { C0: [{ x: 0 }], C1: [{ x: 1 }] };
+
+    const maxIteration = 15;
+
+    for (let t = 1; t <= maxIteration; t++) {
+        const isC0TooFar = kangarooPositions.C0[t - 1].x > kangarooPositions.C1[t - 1].x;
+
+        kangarooPositions.C0[t] = {
+            x: !isC0TooFar
+                ? (kangarooPositions.C0[t - 1]?.x ?? 0) + s
+                : (kangarooPositions.C0[t - 1]?.x ?? 0) - s,
+        };
+        kangarooPositions.C1[t] = { x: C1(t) };
+
+        if (kangarooPositions.C0[t].x === kangarooPositions.C1[t].x) break;
+    }
+}
+
+function drawKangaroos() {
+    if (!kangarooPositions) return;
+
+    const pointRadius = 8;
+    const lineAlpha = 0.6;
+
+    const C0Color = "184, 134, 11";
+    const C1Color = "30, 144, 255";
+
+    ctx.fillStyle = `rgb(${C1Color})`;
+    ctx.setLineDash([5]);
+
+    ctx.beginPath();
+    kangarooPositions.C1.forEach((pos, t) => {
+        const { x: canvasX, y: canvasY } = axisToCanvas({ x: pos.x, y: t });
+
+        ctx.arc(canvasX, canvasY, pointRadius, 0, Math.PI * 2);
+    });
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.strokeStyle = `rgba(${C1Color}, ${lineAlpha})`;
+    kangarooPositions.C1.forEach((pos, t) => {
+        const { x: canvasX, y: canvasY } = axisToCanvas({ x: pos.x, y: t });
+        ctx.lineTo(canvasX, canvasY);
+    });
+    ctx.stroke();
+
+    ctx.fillStyle = `rgb(${C0Color})`;
+    kangarooPositions.C0.forEach((pos, t) => {
+        ctx.beginPath();
+        const { x: canvasX, y: canvasY } = axisToCanvas({ x: pos.x, y: t });
+        ctx.arc(canvasX, canvasY, pointRadius, 0, Math.PI * 2);
+        ctx.fill();
+    });
+
+    ctx.beginPath();
+    ctx.strokeStyle = `rgba(${C0Color}, ${lineAlpha})`;
+    kangarooPositions.C0.forEach((pos, t) => {
+        const { x: canvasX, y: canvasY } = axisToCanvas({ x: pos.x, y: t });
+        ctx.lineTo(canvasX, canvasY);
+    });
+    ctx.stroke();
 }
 
 function axisToCanvas(coords: { x?: number; y?: number }) {
