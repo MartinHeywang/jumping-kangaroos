04f61376059214f2eaa5d27e4714738c50987a15
Martin Heywang <martin.heywang1@gmail.com>
2023-01-13 20:58:35 +0100

    Re-created the table view

--- src/ts/table.ts
+++ src/ts/table.ts
@@ -1,27 +1,31 @@
 import { state } from "./state";
 
-const positionsList = document.querySelector<HTMLUListElement>(
-    ".simul__positions-list"
-)!;
+const stopMessage = document.querySelector<HTMLParagraphElement>(".simul__stop-message")!;
+const positions = document.querySelector<HTMLUListElement>(".simul__positions")!;
 
 export function update() {
-    showPositionsInTable();
-}
+    stopMessage.textContent = state.stopMessage;
 
-function showPositionsInTable() {
-    positionsList.innerHTML = "";
+    positions.innerHTML = "";
 
     for (let t = 0; t < state.positions.C0.length; t++) {
-        const li = document.createElement("li");
-        li.classList.add("simul__position");
 
-        li.innerHTML = `
-            <span>t = ${t}&Delta;t</span
-            ><span>x<sub>0</sub> = ${state.positions.C0[t].x.toFixed(
-                3
-            )}</span><span>x<sub>1</sub> = ${state.positions.C1[t].x}</span>
-        `;
+        const c0Pos = state.positions.C0[t];
+        const c1Pos = state.positions.C1[t];
 
-        positionsList.appendChild(li);
+        const position = document.createElement("li");
+        position.classList.add("simul__position");
+
+        const tSpan = document.createElement("span");
+        const x0Span = document.createElement("span");
+        const x1Span = document.createElement("span");
+
+        tSpan.innerHTML = `${t}&Delta;t`;
+        x0Span.innerHTML = `x<sub>0</sub> = ${c0Pos.x.toFixed(2)}`;
+        x1Span.innerHTML = `x<sub>1</sub> = ${c1Pos.x.toFixed(2)}`;
+
+        position.append(tSpan, x0Span, x1Span);
+
+        positions.appendChild(position);
     }
 }
